6c24de88959794e27d04e39d408c62a973eb3836
Cristian Balaj <cristian.balaj@osf.digital>
2023-03-23 15:34:31 +0200

    FORMS-31 Fixed cookie reported issue.Added SameSite=None attribute to forterToken in cookies.

--- Controller/Index/Sessions.php
+++ Controller/Index/Sessions.php
@@ -9,6 +9,9 @@ use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterf
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
 /**
  * Class Validations
@@ -22,19 +25,43 @@ class Sessions extends Action implements HttpPostActionInterface, CsrfAwareActio
     protected $abstractApi;
 
     /**
+     * @var CookieManagerInterface
+     */
+    protected $cookieManager;
+
+    /**
+     * @var CookieMetadataFactory
+     */
+    protected $cookieMetadataFactory;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
      * @method __construct
-     * @param  Context     $context
-     * @param  AbstractApi $abstractApi
-     * @param  Session     $customerSession
+     * @param Context $context
+     * @param AbstractApi $abstractApi
+     * @param Session $customerSession
+     * @param CookieManagerInterface $cookieManager
+     * @param CookieMetadataFactory $cookieMetadataFactory
+     * @param StoreManagerInterface $storeManager
      */
     public function __construct(
         Context $context,
         AbstractApi $abstractApi,
-        Session $customerSession
+        Session $customerSession,
+        CookieManagerInterface $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory,
+        StoreManagerInterface $storeManager
     ) {
         parent::__construct($context);
         $this->abstractApi = $abstractApi;
         $this->customerSession = $customerSession;
+        $this->cookieManager = $cookieManager;
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -45,6 +72,21 @@ class Sessions extends Action implements HttpPostActionInterface, CsrfAwareActio
         try {
             $forterToken = $this->getRequest()->getHeader('Forter-Token');
             if ($forterToken) {
+                $baseDomain = $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB, true);
+                $parsedUrl = parse_url($baseDomain);
+                $cookieDomain = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
+
+                // Create the cookie metadata
+                $cookieMetadata = $this->cookieMetadataFactory->createPublicCookieMetadata()
+                    ->setDomain($cookieDomain)
+                    ->setPath('/')
+                    ->setSecure(true)
+                    ->setHttpOnly(true)
+                    ->setSameSite('None');
+
+                // Set the forterToken cookie
+                $this->cookieManager->setPublicCookie('forterToken', $forterToken, $cookieMetadata);
+
                 $this->customerSession->setForterToken($forterToken);
             }
 
